07531c988e69bfc50cc377cfba04ad2bd4a676eb
Nabiha Mahboob <108702870+NabihaMahboob@users.noreply.github.com>
2024-11-02 13:03:06 -0400

    Delete live.js

--- live.js
+++
@@ -1,5 +0,0 @@
-<svg id="fundraising-thermometer-118" width="800" height="1050"></svg>
-<script>
-  var fundraising_thermometer_118 = {"layout":"1","fill-color":"rgba(231, 97, 81, 1)","goal-amount":"10000","progress-amount":"2500"};
-</script>
-<script for="fundraising-thermometer-118" type="text/javascript" src="https://d22knjn4n6hjqd.cloudfront.net/thermometer/embed.js"></script>
